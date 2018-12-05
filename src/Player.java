14d5f1a875e6ae0fb7559a8457ba17e940bc1975
ibrahimmunir14 <im318@ic.ac.uk>
2018-12-05 15:42:27 +0000

    Working on implementation of getAllValidMoves.

--- src/Player.java
+++ src/Player.java
@@ -37,15 +37,66 @@ public class Player {
         }
       }
     }
-    Square[] allPawns2 = new Square[index];
-    for (int i = 0; i < index; i++) {
-      allPawns2[i] = allPawns[i];
-    }
-    return allPawns2;
+    return Util.ShrinkSquaresArray(allPawns, index);
   }
 
   public Move[] getAllValidMoves() {
-    return null;
+    Move[] allMoves = new Move[14];
+    int index = 0;
+    for (Square pawn : getAllPawns()) {
+      if (colour == Colour.WHITE ? pawn.getY() == 7 : pawn.getY() == 0) { // pawn on last line
+        continue;
+      }
+      if (colour == Colour.WHITE ? pawn.getY() == 6 : pawn.getY() == 1) { // pawn on penultimate line
+        allMoves[index] = new Move(pawn, board.getSquare(pawn.getX(), (colour == Colour.WHITE ? 7 : 0)), false, false);
+        index++;
+        continue;
+      }
+      Square sqInFront = board.getSquare(pawn.getX(), (colour == Colour.WHITE ? pawn.getY()+1 : pawn.getY()-1));
+      if (sqInFront.occupiedBy() == Colour.NONE) {
+        // there is an empty square in front
+        allMoves[index] = new Move(pawn, sqInFront, false, false);
+        index++;
+        if (colour == Colour.WHITE ? pawn.getY() == 1 : pawn.getY() == 6) {
+          Square sq2InFront = board.getSquare(pawn.getX(), (colour == Colour.WHITE ? pawn.getY()+2 : pawn.getY()-2));
+          if (sq2InFront.occupiedBy() == Colour.NONE) {
+            // this pawn can play a starting move two squares forward
+            allMoves[index] = new Move(pawn, sq2InFront, false, false);
+            index++;
+          }
+        }
+      }
+      if (colour == Colour.WHITE ? pawn.getX() != 7 : pawn.getX() != 0) {
+        // pawn is not on the right edge
+        Square sqForwardRight = board.getSquare((colour == Colour.WHITE ? pawn.getX()+1 : pawn.getX()-1), (colour == Colour.WHITE ? pawn.getY()+1 : pawn.getY()-1));
+        Square sqRight = board.getSquare((colour == Colour.WHITE ? pawn.getX()+1 : pawn.getX()-1), pawn.getY());
+        if (sqForwardRight.occupiedBy() == colour.opponentColour()) {
+          // forward right square can be captured
+          allMoves[index] = new Move(pawn, sqForwardRight, true, false);
+          index++;
+        }
+        else if (sqRight.occupiedBy() == colour.opponentColour() && game.getLastMove().getTo() == sqRight && game.getLastMove().getFrom().getY() == (colour == Colour.WHITE ? sqRight.getY() + 2 : sqRight.getY() - 2)) {
+          // forward right square can be taken by enpassant
+          allMoves[index] = new Move(pawn, sqForwardRight, true, true);
+          index++;
+        }
+      }
+      if (colour == Colour.WHITE ? pawn.getX() != 0 : pawn.getX() != 7) {
+        Square sqForwardLeft = board.getSquare((colour == Colour.WHITE ? pawn.getX()-1 : pawn.getX()+1), (colour == Colour.WHITE ? pawn.getY()+1 : pawn.getY()-1));
+        Square sqLeft = board.getSquare((colour == Colour.WHITE ? pawn.getX()-1 : pawn.getX()+1), pawn.getY());
+        if (sqForwardLeft.occupiedBy() == colour.opponentColour()) {
+          // forward left square can be captured
+          allMoves[index] = new Move(pawn, sqForwardLeft, true, false);
+          index++;
+        }
+        else if (sqLeft.occupiedBy() == colour.opponentColour() && game.getLastMove().getTo() == sqLeft && game.getLastMove().getFrom().getY() == (colour == Colour.WHITE ? sqLeft.getY() + 2 : sqLeft.getY() - 2)) {
+          // forward left square can be taken by enpassant
+          allMoves[index] = new Move(pawn, sqForwardLeft, true, true);
+          index++;
+        }
+      }
+    }
+    return Util.ShrinkMovesArray(allMoves, index);
   }
 
   public boolean isPassedPawn(Square square) {
