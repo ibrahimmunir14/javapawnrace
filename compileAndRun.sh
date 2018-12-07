5f09440b68fc549b8501ee592391d98c90c6fe5a
Ibrahim Munir-Zubair <im318@ic.ac.uk>
2018-12-07 18:07:21 +0000

    compileAndRun.sh and runGame.sh executables to compile/run tests or play PvC respectively.

--- compileAndRun.sh
+++ compileAndRun.sh
@@ -3,5 +3,5 @@
 # cd into the directory containing this script
 cd "$(dirname "$0")"
 
-javac -g -d out -cp "./lib/*" -sourcepath src:test src/*.java test/*.java
+javac -g -d out -cp "./lib/*" -sourcepath src:test src/*.java test/*.java -Xlint:deprecation
 java -cp "./lib/*:out" org.junit.runner.JUnitCore PawnRaceTest
