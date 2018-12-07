f51383f548bacd8ef5fad81810f07f8dc9923c03
Ibrahim Munir-Zubair <im318@ic.ac.uk>
2018-12-07 17:55:18 +0000

    Added colourString to Colour class, as a word representation of the colour.

--- src/Colour.java
+++ src/Colour.java
@@ -1,16 +1,21 @@
 public enum Colour {
-  BLACK('B'), //(char)(9823)
-  WHITE('W'), //(char)(9817)
-  NONE('.');
+  BLACK('B', "Black"), //(char)(9823)
+  WHITE('W', "White"), //(char)(9817)
+  NONE('.', "None");
 
   private final char colourRep;
+  private final String colourString;
 
-  private Colour(char colourRep) {
+  private Colour(char colourRep, String colourString) {
     this.colourRep = colourRep;
+    this.colourString = colourString;
   }
 
   public char colourRep() {
     return this.colourRep;
+  }
+  public String colourString() {
+    return this.colourString;
   }
 
   public Colour opponentColour() {
